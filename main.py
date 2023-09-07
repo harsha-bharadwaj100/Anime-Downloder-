0e0a13be26fb065f245a10232901656b316e3509
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-09-07 11:09:45 +0530

    added inputs!

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+print("Supported site: https://www5.gogoanimes.fi", sep="\n")
+animeLink = input("Enter Anime Link: ").strip().replace("category/", "")
+episodeNumber = input("Enter Episode: ").strip()
+episodeLink = animeLink + f"-episode-{episodeNumber}"
+print(f"sanitized {animeLink = }", f"{episodeLink = }", sep="\n")
+
 subprocess.Popen("Start-Chrome-Instance.bat")
-# time.sleep(1)
 # Initialize the WebDriver (make sure you have the appropriate driver for your browser installed)
 option = webdriver.ChromeOptions()
 option.add_experimental_option("debuggerAddress", "localhost:9222")
-# time.sleep(1)
 driver = webdriver.Chrome(options=option)
 driver.set_page_load_timeout(10)
-# time.sleep(1)
 # Open Episode Page
 while 1:
     try:# Sometimes when you open link for the first time after turning on pc or restarting it kinda keeps loading forever...
-        driver.get("https://www5.gogoanimes.fi/one-piece-episode-64")# That's why I set page load timeout to say 10 seconds...
+        driver.get(episodeLink)# That's why I set page load timeout to say 10 seconds...
     except TimeoutException as err:# When the 10 seconds finish it will raise a timeout error and we will catch it...
         print(err)# and then try again...
         continue# if the same error occurs then just continue...
@@ -45,7 +48,7 @@ print("FOUND THE ELEMENT!!!!!!!!!!!!!!!!!!!!!!!!!")
 # Link opens in a new tab, click on download link
 downloadLink = driver.find_element(By.CSS_SELECTOR, '#content-download > div:nth-child(1) > div:nth-child(3) > a')
 print("Clicking the link: ", downloadLink.get_attribute("href"))
-# downloadLink.click()
+downloadLink.click()
 # Close the browser
 driver.quit()
 
